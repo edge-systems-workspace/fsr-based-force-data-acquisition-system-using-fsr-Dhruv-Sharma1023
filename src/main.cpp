4e048d140fb26f1af7d259a26c3e4e80f53cea24
Dhruv Sharma <dhruv.sharmauni@gmail.com>
2026-02-27 23:33:26 +0530

    TODO 5: cast analogRead result to int for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ void loop() {
 
     // TODO 5:
     // Read analog value from FSR
-    fsrValue = analogRead(FSR_PIN);
+    fsrValue = (int)analogRead(FSR_PIN);
 
     // TODO 6:
     // Print raw ADC value
